cf64be7462d1b704500673c4595485fe580641ef
sidfubo1 <sidfubo1@gmail.com>
2018-07-18 19:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function scuberGreetingForFeet(x){
   if (x<=400){
     return 'This one is on me!';
   }
-  else if (x>2000){
+  else if (x>2000 && x<=2500){
     return 'I will gladly take your thirty bucks.';
   }
+  else if
 }
