15a4f438079d1167730c000710e5a4a2ba110ea6
sidfubo1 <sidfubo1@gmail.com>
2018-07-18 19:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,7 @@ function ternaryCheckCity(y){
   }
 }
 
-//
+//switchOnCharmFromTip() — Use a switch statement to return a different response based on the generosity of the passenger's tip.
+function switchOnCharmFromTip(){
+  
+}
